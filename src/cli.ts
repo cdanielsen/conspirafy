9f6b3a3e1f74ae19323a108814e96e5db2abce28
Christian Danielsen <cdanielsen@mural.co>
2024-11-12 21:37:56 -0800

    refactor: yargs => commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,55 +1,53 @@
 #! /usr/bin/env node
 import { pipeline } from 'node:stream/promises';
 import { stdin, stdout } from 'node:process';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
 import { conspirafy } from './conspirafy.js';
 import { Transform } from 'node:stream';
+import { Command } from 'commander';
+const program = new Command('conspirafy');
 
-yargs(hideBin(process.argv))
-  .scriptName('conspirafy')
-  .command(
-    '$0 [text] [options]',
-    'Conspirafy some text!',
-    (yargs) => {
-      yargs.option('stdin', {
-        alias: 's',
-        type: 'boolean',
-        description: 'Read from stdin instead of passing text',
-      });
-      yargs.positional('text', {
-        description: 'A required text argument (unless --stdin is passed)',
-        type: 'string',
-        conflicts: 'stdin',
-      });
-      yargs.check((argv) => {
-        if (!argv.text && !argv.stdin) {
-          throw new Error('You must pass a text argument if not using --stdin');
-        }
-        return true;
-      });
-    },
-    async ({ text, stdin: stdinArg }) => {
-      // Read from stdin if the --stdin or -s flag is passed
-      if (stdinArg) {
-        const conspirafyText = new Transform({
-          transform(chunk, encoding, callback) {
-            this.push(conspirafy(chunk.toString()));
-            callback();
-          },
-        });
-        try {
-          await pipeline(stdin, conspirafyText, stdout);
-          process.exit(0);
-        } catch (err) {
-          console.error('Failed to conspirafy stdin =/', err);
-          process.exit(1);
-        }
-      }
-      const result = conspirafy(text);
-      process.stdout.write(result);
-    },
-  )
-  .example('$0 "The moon landing was a hoax"')
-  .help()
+interface Options {
+  stdin: boolean;
+}
+
+const runCommand = async (text: string | undefined, opts: Options) => {
+  console.log({ text });
+  if (!text && !opts.stdin) {
+    console.error('You must pass a text argument if not using --stdin');
+    process.exit(1);
+  }
+
+  if (text && opts.stdin) {
+    console.error('You cannot pass a text argument if using --stdin');
+    process.exit(1);
+  }
+
+  if (opts.stdin && !text) {
+    const conspirafyText = new Transform({
+      transform(chunk, _, callback) {
+        this.push(conspirafy(chunk.toString()));
+        callback();
+      },
+    });
+    try {
+      await pipeline(stdin, conspirafyText, stdout);
+      process.exit(0);
+    } catch (err) {
+      console.error('Failed to conspirafy stdin =/', err);
+      process.exit(1);
+    }
+  }
+
+  if (!opts.stdin && text) {
+    const result = conspirafy(text);
+    process.stdout.write(result);
+  }
+};
+
+program
+  .version('1.0.2', '-v, --vers', 'Output the current version')
+  .description('Conspirafy some text!')
+  .argument('[text]', 'The text to conspirafy')
+  .option('-s, --stdin', 'Read from stdin instead of passing text')
+  .action(runCommand)
   .parse();
